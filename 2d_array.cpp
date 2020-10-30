360a083a147317f4456db170428f3009a6b25504
iamaditya <adity1s2h@gmail.com>
2020-10-30 18:29:23 +0530

    interaction added

--- 2d_array.cpp
+++ 2d_array.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 int main(){
     int arr[3][3]; // first 3 is for row whereas 2nd 3 is for columns 
+    cout<<"Enter int values for arr [3][3]:\n";
     for (int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             cin>>arr[i][j];
         }
     }
+    cout<<"Array you entered :\n";
     for (int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             cout<<arr[i][j]<<" ";
