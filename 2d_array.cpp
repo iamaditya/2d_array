68883cd927ad5f6e7813892daf3346700a325cfc
iamaditya <adity1s2h@gmail.com>
2020-10-30 18:19:09 +0530

    array added using for loops

--- 2d_array.cpp
+++ 2d_array.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 int main(){
-    
+    int arr[3][3]; // first 3 is for row whereas 2nd 3 is for columns 
+    for (int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cin>>arr[i][j];
+        }
+    }
+    for (int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
